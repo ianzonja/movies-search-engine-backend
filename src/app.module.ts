763690e073d9b1c24dc7faefd4fc6641e25146ce
ianzonja <ianzonja@gmail.com>
2022-01-08 19:58:25 +0100

    typeorm sqlite3 setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from 'ormconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(config),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
